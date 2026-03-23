c84cae3470ca24fa2cb710387d845494091066ca
Bernardo Paz de Freitas <125703112+b3rnard0p@users.noreply.github.com>
2026-03-23 16:08:36 -0300

    Add files via upload

---
+++ Estudos/TryParse/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace TryParse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Pessoa pessoa = new Pessoa();
+
+            Console.Write("Digite o número o cpf (apenas números): ");
+
+            while (true)
+            {
+                if (long.TryParse(Console.ReadLine(), out pessoa.cpf))
+                {
+                    Console.WriteLine("Cadastrado com sucesso!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Entrada inválida!");
+                    Console.Write("Por favor, digite novamente: ");
+                }
+            }
+        }
+    }
+}
