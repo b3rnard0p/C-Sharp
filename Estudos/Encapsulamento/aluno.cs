2fe5690ce346a166c5bcb6a3c1e8aa5530e6b985
Bernardo Paz de Freitas <125703112+b3rnard0p@users.noreply.github.com>
2026-03-23 16:53:44 -0300

    Add files via upload

---
+++ Estudos/Encapsulamento/aluno.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laços
+{
+    internal class Aluno
+    {
+        public string Nome { get; set; }
+
+        private double _nota;
+
+        public double Nota
+        {
+            get { return _nota; }
+            set
+            {
+                if (value < 10)
+                {
+                    Console.WriteLine("Erro: A nota não pode ser menor que 10!");
+                    _nota = 10;
+                }
+                else
+                {
+                    _nota = value;
+                }
+            }
+        }
+
+        public string Matricula { get; private set; }
+
+        public Aluno(string numeroMatricula)
+        {
+            Matricula = numeroMatricula;
+        }
+    }
+}
